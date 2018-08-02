010517caa832d454f9a172d311e3ab24383f6743
Tarek Khoury <t.khoury@applicaster.com>
2018-08-02 22:43:28 +0300

    initial

---
+++ src/main/java/com/applicaster/cleengloginplugin/views/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.applicaster.cleengloginplugin.views
+
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+
+class LoginActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.d("tk", "bla")
+    }
+}
