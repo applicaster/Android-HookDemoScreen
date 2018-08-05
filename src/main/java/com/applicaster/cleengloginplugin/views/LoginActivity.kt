041f8a3b0ef4a294d06ac6b1423b3d8913e4151c
Tarek Khoury <t.khoury@applicaster.com>
2018-08-05 14:04:57 +0300

    initial commit

--- src/main/java/com/applicaster/cleengloginplugin/views/LoginActivity.kt
+++ src/main/java/com/applicaster/cleengloginplugin/views/LoginActivity.kt
@@ -1,14 +1,17 @@
 package com.applicaster.cleengloginplugin.views
 
-import android.app.Activity
 import android.os.Bundle
-import android.util.Log
+import android.support.v7.app.AppCompatActivity
+import com.applicaster.cleengloginplugin.R
+import kotlinx.android.synthetic.main.login_activity.*
 
-class LoginActivity : Activity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
 
-        Log.d("tk", "bla")
+
+
     }
 }
