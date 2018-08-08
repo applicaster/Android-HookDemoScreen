91f6be7eed16fbfc12f47e346a2984aa4e3d6081
Elad Kariti <e.kariti@applicaster.com>
2018-08-08 16:02:16 +0300

    Add resotre layout

--- src/main/java/com/applicaster/cleengloginplugin/views/ResetPasswordActivity.kt
+++ src/main/java/com/applicaster/cleengloginplugin/views/ResetPasswordActivity.kt
@@ -13,24 +13,25 @@ import com.applicaster.util.StringUtil
 import kotlinx.android.synthetic.main.additional_auth.*
 import kotlinx.android.synthetic.main.login_activity.*
 import kotlinx.android.synthetic.main.new_account_activiy.*
+import kotlinx.android.synthetic.main.restore_activity.*
 import kotlinx.android.synthetic.main.user_input.*
 
-class SignUpActivity : BaseActivity() {
+class ResetPasswordActivity : BaseActivity() {
 
     override fun getContentViewResId(): Int {
-        return R.layout.new_account_activiy;
+        return R.layout.restore_activity;
     }
 
     override fun customize() {
         super.customize()
-        CustomizationHelper.updateTextView(this, R.id.enter_details, NEW_ACCOUNT_TITLE)
-        CustomizationHelper.updateTextView(this, R.id.sign_up_action_text, SIGN_IN_LABEL_TEXT)
-        CustomizationHelper.updateTextView(this, R.id.sign_up_text, ALREADY_HAVE_ACCOUNT_HINT)
+
+        CustomizationHelper.updateTextView(this, R.id.restore_title, RESTORE_TITLE)
+        CustomizationHelper.updateTextView(this, R.id.restore_description, RESTORE_DESCRIPTION)
 
         CustomizationHelper.updateEditTextView(this, R.id.input_email, DEFAULT_1, true);
         CustomizationHelper.updateEditTextView(this, R.id.input_password, DEFAULT_2, true);
         CustomizationHelper.updateTextView(this, R.id.forgot_password, RESET_PASSWORD_ACTION_TEXT)
-        CustomizationHelper.updateButtonViewText(this, R.id.action_button, SIGN_UP_BUTTON)
+        CustomizationHelper.updateButtonViewText(this, R.id.action_button, RESTORE_BUTTON)
 
         updateViews();
 
@@ -38,21 +39,21 @@ class SignUpActivity : BaseActivity() {
             val user = this.getUserFromInput() ?: return@setOnClickListener
 
             this.showLoading()
+            //should we register???? from restore, how can i restore the acount?
             CleengManager.register(user, this) { status: WebService.Status, response: String? ->
                 this.dismissLoading()
                 //continue flow
             }
         }
 
-        sign_in_hint.setOnClickListener{
-            LoginActivity.launchLogin(this);
-            this.finish()
+        restore_bottom_bar_container.setOnClickListener{
+            //clicking on the bottom bar in restore
+            // What should we do here???????
         }
 
-        bottom_bar_container_new_account.setOnClickListener{
-            RestoreActivity.launchRestoreActivity(this)
-            this.finish()
-       }
+        forgot_password.setOnClickListener{
+            //go to reset password
+        }
     }
 
     fun updateViews() {
@@ -60,12 +61,11 @@ class SignUpActivity : BaseActivity() {
             facebook_button.visibility = View.GONE;
             google_button.visibility = View.GONE;
         }
-        forgot_password.visibility = View.GONE;
     }
 
     companion object {
-        fun launchSignUpActivity(context: Context) {
-            context.startActivity(Intent(context, SignUpActivity::class.java))
+        fun launchRestoreActivity(context: Context) {
+            context.startActivity(Intent(context, ResetPasswordActivity::class.java))
         }
     }
 }
